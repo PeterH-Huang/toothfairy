ebb4f266c0839e2c6cc00f3aca775c1ce0fdb2b1
Sawyer Murphy <smurp091@uottawa.ca>
2022-04-14 16:34:11 -0400

    Testing database connectivity

--- addPatientInfoPage.php
+++ addPatientInfoPage.php
@@ -68,6 +68,11 @@
         <?php 
             $connection = pg_connect("host=ec2-52-21-136-176.compute-1.amazonaws.com
             dbname=dae350stsd51e2 user=anbtnmsnsbhumz password=7cd60d6bc02b2a802a4b0e107994f85faad6721a0557f67b0903832fa04bd137");
+            if($connection) {
+                echo 'connected';
+             } else {
+                 echo 'there has been an error connecting';
+             }
 
             if(isset($_POST['submit'])){
                 //if($_POST['submit'] == 'Submit') {
