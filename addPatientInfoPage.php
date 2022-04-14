9e5cc5b269b34edf253d92a60052e4702fd30588
Sawyer Murphy <smurp091@uottawa.ca>
2022-04-14 15:58:21 -0400

    Testing query

--- addPatientInfoPage.php
+++ addPatientInfoPage.php
@@ -103,19 +103,19 @@
                     $insuranceNumber = $_POST['insuranceNumber'];
                     $userType = $_POST['userType'];
 
-                    if($firstName != null && $lastName != null && $middleName != null && $ssn != null && $id != null && $houseNumber != null && $streetName != null && $city != null && $province != null && $gender != null && $age != null && $emailAddress != null && $phoneNumber != null && $birthDay != null && $birthMonth != null && $birthYear != null){
+                    //if($firstName != null && $lastName != null && $middleName != null && $ssn != null && $id != null && $houseNumber != null && $streetName != null && $city != null && $province != null && $gender != null && $age != null && $emailAddress != null && $phoneNumber != null && $birthDay != null && $birthMonth != null && $birthYear != null){
                         $queryOne = pg_query($connection, "insert into users values ($ssn, $houseNumber, $streetName, $city, $province, $firstName, $middleName, $lastName, $gender, $age, $insuranceNumber, $emailAddress, $birthDay, $birthMonth, $birthYear, $userType, $phoneNumber)"); //Insert Query
                         
                         /*if($userType == 0) {
                             $queryTwo = pg_query($connection, "insert into patient values($id, $ssn, $dependantOneFirstName, $dependantOneMiddleName, $dependantOneLastName, $dependantTwoFirstName, $dependantTwoMiddleName, $dependantTwoLastName, $dependantThreeFirstName, $dependantThreeMiddleName, $dependantThreeLastName,)");
                         }*/
 
-                        if($queryOne /*&& $queryTwo*/) {
+                      //  if($queryOne /*&& $queryTwo*/) {
                             echo "<script type='text/javascript'>alert('Successfull!');</script>";
-                        }
-                    } else {
-                        echo "<script type='text/javascript'>alert('fail');</script>";
-                    } 
+                       // }
+                    //} else {
+                    //    echo "<script type='text/javascript'>alert('fail');</script>";
+                    //} 
                 
                 
                 }
