61a552c730fd1c9c79ad306f2448a110f8b123f1
Peter <1337.peterhuang@gmail.com>
2022-04-15 15:26:52 -0400

    weqjhbfkhijksl

--- patientMedicalHistory.php
+++ patientMedicalHistory.php
@@ -57,7 +57,7 @@
                     
                     }
                     
-                    
+                    //
                      
                     if($query) {
                         echo $query;
