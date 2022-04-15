d8be6fb40b0aaa2cd451fae4216c2a552b2c77cc
Peter <1337.peterhuang@gmail.com>
2022-04-15 15:10:27 -0400

    pls work

--- patientMedicalHistory.php
+++ patientMedicalHistory.php
@@ -25,7 +25,7 @@
         <div class="welcome" id="welcome">
             <p>Your Medical History:</p>
         </div> <br>
-<table>
+
         <?php 
             $connection = pg_connect("host=ec2-52-21-136-176.compute-1.amazonaws.com
             dbname=dae350stsd51e2 user=anbtnmsnsbhumz password=7cd60d6bc02b2a802a4b0e107994f85faad6721a0557f67b0903832fa04bd137");
@@ -43,12 +43,8 @@
                     if(pg_num_rows($query) == 1){
                         $result = pg_query($connection, "SELECT recordid, previousprocedureone,previousproceduretwo,previousprocedurethree,previousprocedurefour,previousprocedurefive FROM records WHERE recordpatientid = '$record'");
                         while ($row = pg_fetch_row($result)){
+                            echo "<table>";
                             echo "<tr>";
-                            echo "<td> </td>";
-                            echo "<td> </td>";
-                            echo "<td> </td>";
-                            echo "<td> </td>";
-                            echo "<td> </td>";
                             echo "<td> <p align=center>$row[0] </p></td>";
                             echo "<td> <p align=center>$row[1] </p></td>";
                             echo "<td> <p align=center>$row[2] </p></td>";
@@ -56,6 +52,7 @@
                             echo "<td> <p align=center>$row[4] </p></td>";
                             echo "<td> <p align=center>$row[5] </p></td>";
                             echo "</tr>";
+                            echo "</table>"
                         }
                     
                     }
@@ -74,7 +71,7 @@
         ?>
 
 
-        </table>
+        
 
 
     </body>
