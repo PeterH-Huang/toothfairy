92063420543eb1bf6e792903bc43c7bb24d60f95
Sawyer Murphy <smurp091@uottawa.ca>
2022-04-14 20:46:11 -0400

    More testing

--- addPatientInfoPage.php
+++ addPatientInfoPage.php
@@ -75,7 +75,8 @@
                  echo 'there has been an error connecting';
              }
 
-            if(!isset($_POST['confirm'])){
+            if(isset($_POST['confirm'])){
+                    echo "MADE IT";
                 //if($_POST['submit'] == 'Submit') {
                     //$property_agreementID = rand(8300,9500); 
                     $userfirstName = /*(string)*/ $_POST['userFirstName'];
