0c6c582b8e8d5405868acd48772e06a8d70efa98
Nathan Dizon-Mapula <ndizonmapula@gmail.com>
2022-04-15 16:00:10 -0400

    trying to save patient IDs into a table

--- appointments.php
+++ appointments.php
@@ -21,7 +21,7 @@
         </div>
         <br>
 
-        <p> Welcome
+        <p> Welcome Dentist
             <?php
             $connection = pg_connect("host=ec2-52-21-136-176.compute-1.amazonaws.com
             dbname=dae350stsd51e2 user=anbtnmsnsbhumz password=7cd60d6bc02b2a802a4b0e107994f85faad6721a0557f67b0903832fa04bd137");
@@ -33,7 +33,23 @@
             $connection = pg_connect("host=ec2-52-21-136-176.compute-1.amazonaws.com
                         dbname=dae350stsd51e2 user=anbtnmsnsbhumz password=7cd60d6bc02b2a802a4b0e107994f85faad6721a0557f67b0903832fa04bd137");
             $queryOne = pg_query($connection, "SELECT appointmentpatientid FROM appointment WHERE appointmentemployeeid = dentistID AND appointmentdateday = dateDay AND appointmentdatemonth = dateMonth AND  appointmentdateyear = dateYear");
-            echo "<br> <$queryOne> <br>";
+
+            //putting everything into an array
+            if(pg_num_rows($query) == 1){
+                $result = pg_query($connection, "SELECT appointmentpatientid FROM appointment WHERE appointmentemployeeid = dentistID AND appointmentdateday = dateDay AND appointmentdatemonth = dateMonth AND  appointmentdateyear = dateYear");
+                while ($row = pg_fetch_row($result)){
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<td> <p align=center>$row[0] </p></td>";
+                    echo "<td> <p align=center>$row[1] </p></td>";
+                    echo "<td> <p align=center>$row[2] </p></td>";
+                    echo "<td> <p align=center>$row[3] </p></td>";
+                    echo "<td> <p align=center>$row[4] </p></td>";
+                    echo "<td> <p align=center>$row[5] </p></td>";
+                    echo "</tr>";
+                    echo "</table>";
+                }
+            //echo "<br> <$queryOne> <br>";
             ?>
         </p><br>
 
